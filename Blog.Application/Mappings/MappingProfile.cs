b271a9546276711b2007659976a7ee619504516f
Aditya Pachupate <adityapachupate@gmail.com>
2025-09-23 12:14:34 +0530

    feat(application): add AutoMapper with mapping profiles
    - Added AutoMapper package reference in Blog.Application.csproj
    - Added FluentValidation and MediatR package references
    - Created MappingProfile.cs to define mappings between domain entities and DTOs
    - Implemented custom property transformations for authors and users

---
+++ Blog.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace Blog.Application.Mappings
+{
+    using AutoMapper;
+    using Blog.Application.DTOs;
+    using Blog.Domain.Entities;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Post, PostDto>()
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => $"{src.Author.FirstName} {src.Author.LastName}"))
+                .ForMember(dest => dest.AuthorProfilePictureUrl, opt => opt.MapFrom(src => src.Author.ProfilePictureUrl));
+
+            CreateMap<PostDto, Post>();
+
+            CreateMap<Comment, CommentDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src =>$"{src.User.FirstName} {src.User.LastName}"));
+        }
+    }
+}
